2628245c0b7eb0b6e95dfb4f8a639a9c6ca61d7f
Andrea Massa <andreamassa1999@gmail.com>
2023-06-15 15:47:37 -0400

    Started working on views

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const ejs = require('ejs');
 
-const server = express();
 
+const server = express();
+server.set('view engine', 'ejs');
+server.set('views', __dirname + '/views');
+server.use(express.static(__dirname + '/public'));
 
 server.listen(3000, () => {
     console.log('SERVER LISTENING ON PORT 3000')
 })
 
+
+
+
+
+const notes = [
+    {
+        title: 'Workout',
+        content: 'Push Workout',             
+    },
+    {
+        title: 'Recipes',
+        content: 'This is a veriety of recipes',             
+    },
+]
+
 server.get('/', (req, res) => {
-    res.send('Working');
+    res.render('home', {notes});
 })
