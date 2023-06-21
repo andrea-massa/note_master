84c2c54b6f9410e7ae1cfd016e2f01446fb5e017
Andrea Massa <andreamassa1999@gmail.com>
2023-06-21 12:27:21 -0400

    Front End Bug Fixing and minor code refactoring

--- public/scripts/note.js
+++ public/scripts/note.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
 
-    // Clicking on the Edit Button renderes the edit menu visible and actual note invisible
+    // Clicking on the Edit Button renderes the edit menu visible and all the other notes invisible
     $('.note-edit-button').click(function (e) {         
-        $noteDiv = $( this ).parent().parent();
-        $noteEditDiv = $noteDiv.next();
+        // Show the current note in 'Edit Mode'
+        $( this ).parent().parent().next().removeClass('hidden');
 
-        $('.note').addClass('hidden');
-        $noteEditDiv.removeClass('hidden');
+        //Hide all other notes
+        $('.note').each(function (index, element) {
+            $(this).addClass('hidden');            
+        });                
     });
 });
